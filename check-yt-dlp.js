1a8c0a09178a8ccffa92a9b926b207b5fb9501c5
hassan razzaq <hassanrazzaq200@gmail.com>
2025-01-13 02:16:21 +0500

    Updated check-yt-dlp.js script

--- check-yt-dlp.js
+++ check-yt-dlp.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const childProcess = require('child_process');
 
 const ytDlpExecutable = 'yt-dlp.exe';
 
 if (!fs.existsSync(ytDlpExecutable)) {
-    console.log('Error: yt-dlp.exe file not found.');
-    process.exit(1);
+    console.log('Downloading yt-dlp.exe...');
+    childProcess.execSync(`curl -L -o ${ytDlpExecutable} https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe`);
+    console.log('yt-dlp.exe downloaded successfully.');
 }
